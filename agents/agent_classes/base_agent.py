8cd9f11b22703af2396687245ed514850a5e765e
Steve Wang <stevewang@alumni.stanford.edu>
2025-10-11 17:17:37 -0400

    Add doc strings for BaseAgent

--- agents/agent_classes/base_agent.py
+++ agents/agent_classes/base_agent.py
@@ -10,7 +10,20 @@ class BaseAgent:
         instructions: str=None,
         name: str=None,
         description: str=None,
-        func: callable=None):
+        func: callable=None
+        ):
+        
+        '''
+        BaseAgent is the parent class for the ActionPlanningAgent, KnowledgeAugmentedPromptAgent, and EvaluationAgent.
+        This is the base initiation method.
+        
+        Args:
+        openai_instance (OpenAI): an OpenAI client
+        instructions (str): the instructions for the agent, i.e., the system prompt
+        name (str): name of the agent
+        description (str): description of the agent
+        func (callable): optional function to be called by the agent.
+        '''
         
         self.openai_instance = openai_instance
         self.instructions = instructions
@@ -24,8 +37,21 @@ class BaseAgent:
         self, 
         input: str, 
         model: str="gpt-3.5-turbo", 
-        temperature: float=0.):
-        # Generate a response using the OpenAI API
+        temperature: float=0.
+        ) -> str:
+        
+        '''
+        Makes a call to OpenAI's responses API and returns the output_text of the response object
+        
+        Args:
+        input (str): the user prompt
+        model (str): the OpenAI model to be used
+        temperature (float): value for how creative the response should be; note that this is not available for gpt-5
+        
+        Returns:
+        string representing the output from the OpenAI response
+        '''
+
         try:
             if model in MODELS_WITHOUT_TEMPERATURE:
                 response = self.openai_instance.responses.create(
