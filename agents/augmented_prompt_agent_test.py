4880a0790a90fdc4a8ef24591330b0aea267d50c
Steve Wang <stevewang@alumni.stanford.edu>
2025-10-13 16:53:19 -0400

    Make augmented_prompt_agent_test more robust

--- agents/augmented_prompt_agent_test.py
+++ agents/augmented_prompt_agent_test.py
@@ -7,8 +7,14 @@ input = "What is the capital of France?"
 instructions = "You are a college professor; your answers always start with: 'Dear students,'"
 
 augmented_prompt_agent = BaseAgent(openai_instance, instructions)
-augmented_agent_response = augmented_prompt_agent.get_response_text(input)
+augmented_agent_response = None
+num_iterations = 0
+while not augmented_agent_response or "Dear students" != augmented_agent_response[:len("Dear students")]:
+    augmented_agent_response = augmented_prompt_agent.get_response_text(input)
+    num_iterations += 1
 print(augmented_agent_response)
+print(f"num_iterations: {num_iterations}")
+assert("Dear students" == augmented_agent_response[:len("Dear students")])
 
 # The agent used the LLM's knowledge to answer the prompt. The instructions modified the answer's format.
 # While the instructions assigned a role to the LLM, I don't believe that it affected the substance of the response here,
